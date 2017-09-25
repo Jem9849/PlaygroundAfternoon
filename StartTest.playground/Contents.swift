87c69b36abba6c0dbfc8811e68752bb40226f43f
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-09-25 12:21:43 -0600

    Trying to understand strings by testing.

--- StartTest.playground/Contents.swift
+++ StartTest.playground/Contents.swift
@@ -15,3 +15,19 @@ if (6 > 3)
 }
 
 var thisName :String = "roht"
+///////////////////////////////////// String Testing
+
+var nameOfWho :String
+nameOfWho = "anonymous"
+
+if (nameOfWho == "anonymous")
+{
+    print("Enter a name.")
+    nameOfWho = readLine()
+    countName = nameOfWho.count
+}
+
+else
+{
+    print(nameOfWho "is " + countName + " in length." "That times 5 is " \(countName * 5)
+}
