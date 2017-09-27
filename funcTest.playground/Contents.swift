98f1b4542b4c6c88e067e051770db7213b798ba3
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-09-27 12:13:57 -0600

    Testing insanity and craziness. Basically a bunch of while, if, and testing of func.

--- funcTest.playground/Contents.swift
+++ funcTest.playground/Contents.swift
@@ -15,3 +15,35 @@ public func aBitMoreSimple(name: String) -> Void
 }
 
 aBitMoreSimple(name: "WEIRDO MCGUFFINS")
+
+public func exampleNaming(outerName innerName :String) -> Void
+{
+    print("The inner name is: \(innerName)")
+}
+
+exampleNaming(outerName: "Cody")
+
+public func age() -> Int
+{
+    return (7 % 80)
+}
+
+print("I am not \(age()) years old.")
+
+if(age() < 10)
+{
+    print("Math does not work in swift. It is all a lie.")
+}
+else
+{
+    print("Not sure what you want from me right now.")
+}
+
+var test = 0
+while (test < age())
+{
+    print("WABBAJACK!")
+    test += 1
+}
+
+print("If you have got here then you suck, and should of let it keep runnning.")
