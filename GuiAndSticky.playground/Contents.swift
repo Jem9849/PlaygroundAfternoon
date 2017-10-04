90baec1261fba66b95564d3cec816545a252b1f9
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-10-04 13:51:58 -0600

    Trying to understand pickers (for now erased the code to try again later)

--- GuiAndSticky.playground/Contents.swift
+++ GuiAndSticky.playground/Contents.swift
@@ -7,3 +7,6 @@ var topic = "GUI"
 var myFirstButton = UIButton(frame: CGRect(x:30, y:60, width: 600, height: 120))
 myFirstButton.setTitle("Words", for: .normal)
 myFirstButton.backgroundColor = .blue
+
+
+
