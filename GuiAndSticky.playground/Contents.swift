9e78ef5838209f139e185cdc591c192c212032e3
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-10-04 12:08:29 -0600

    Added img and made a button yay

--- GuiAndSticky.playground/Contents.swift
+++ GuiAndSticky.playground/Contents.swift
@@ -2,4 +2,8 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var topic = "GUI"
+
+var myFirstButton = UIButton(frame: CGRect(x:30, y:60, width: 600, height: 120))
+myFirstButton.setTitle("Words", for: .normal)
+myFirstButton.backgroundColor = .blue
